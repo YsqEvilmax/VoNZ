ce700d85ac9df8c7a7b7aca1ac66bd4bbaa32031
YsqEvilmax <ysq.evilmax@qq.com>
2016-04-23 21:36:11 +1200

    add config.php

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ochfem4hh3y1',
+  'passwordsalt' => '7DzYVBhoe3iz6JLpRsD2TIOA4rDeG3',
+  'secret' => '8SyB5zSREUFXMyyV0qTcXhDhHYF+EDMvGIEcxooSoZTZJ2G4',
+  'trusted_domains' => 
+  array (
+    0 => 'ec2-52-63-247-158.ap-southeast-2.compute.amazonaws.com',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://ec2-52-63-247-158.ap-southeast-2.compute.amazonaws.com',
+  'dbtype' => 'mysql',
+  'version' => '9.0.1.3',
+  'dbname' => 'vonz',
+  'dbhost' => 'mysql.czrhhzselpqu.ap-southeast-2.rds.amazonaws.com',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'pwMS/oe2u5bTbztXX9lfqaS4iBhana',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+  'mail_smtpmode' => 'smtp',
+  'mail_smtpsecure' => 'ssl',
+  'mail_from_address' => 'voiceofnewzealand',
+  'mail_domain' => 'gmail.com',
+  'mail_smtpauth' => 1,
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_smtphost' => 'smtp.gmail.com',
+  'mail_smtpport' => '465',
+  'mail_smtpname' => 'voiceofnewzealand@gmail.com',
+  'mail_smtppassword' => 'ai251917',
+);
+
