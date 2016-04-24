8e44cc5a6385f3905f9d2d4e482f8c2f4eacc196
YsqEvilmax <ysq.evilmax@qq.com>
2016-04-24 13:21:41 +1200

    add environmental variables

--- setting.sh
+++ setting.sh
@@ -2,10 +2,12 @@
 #wget -O apps/activity/appinfo/info.xml https://raw.githubusercontent.com/YsqEvilmax/vonz/master/apps/activity/appinfo/info.xml
 #wget -O apps/gallery/appinfo/info.xml https://raw.githubusercontent.com/YsqEvilmax/vonz/master/apps/gallery/appinfo/info.xml
 
+VONZ_ROOT="/var/www/html"
+
 # disable activity app by defualt
-sed -i "s/<default_enable\/>/ /g" apps/activity/appinfo/info.xml
+sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/activity/appinfo/info.xml
 
 # disable gallery app by defualt
-sed -i "s/<default_enable\/>/ /g" apps/gallery/appinfo/info.xml
+sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/gallery/appinfo/info.xml
 
 exec /entrypoint.sh
