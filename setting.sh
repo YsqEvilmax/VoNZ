c7550e2e316951ed3a069232f3b78ade9e23608c
YsqEvilmax <ysq.evilmax@qq.com>
2016-04-24 13:39:14 +1200

    test exec

--- setting.sh
+++ setting.sh
@@ -5,10 +5,10 @@
 VONZ_ROOT="/var/www/html"
 
 # disable activity app by defualt
-sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/activity/appinfo/info.xml
+#sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/activity/appinfo/info.xml
 
 # disable gallery app by defualt
-sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/gallery/appinfo/info.xml
+#sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/gallery/appinfo/info.xml
 
 exec "/entrypoint.sh"
 exec "$@"
