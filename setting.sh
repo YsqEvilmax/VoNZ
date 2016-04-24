3a216693dfd6aa681d5ddc9817099db7d89c3e96
YsqEvilmax <ysq.evilmax@qq.com>
2016-04-24 13:52:18 +1200

    copy entrypoint to setting

--- setting.sh
+++ setting.sh
@@ -2,13 +2,19 @@
 #wget -O apps/activity/appinfo/info.xml https://raw.githubusercontent.com/YsqEvilmax/vonz/master/apps/activity/appinfo/info.xml
 #wget -O apps/gallery/appinfo/info.xml https://raw.githubusercontent.com/YsqEvilmax/vonz/master/apps/gallery/appinfo/info.xml
 
+set -e
+
+if [ ! -e '/var/www/html/version.php' ]; then
+	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
+	chown -R www-data /var/www/html
+fi
+
 VONZ_ROOT="/var/www/html"
 
 # disable activity app by defualt
-#sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/activity/appinfo/info.xml
+sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/activity/appinfo/info.xml
 
 # disable gallery app by defualt
-#sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/gallery/appinfo/info.xml
+sed -i "s/<default_enable\/>/ /g" $VONZ_ROOT/apps/gallery/appinfo/info.xml
 
-exec "/entrypoint.sh"
 exec "$@"
